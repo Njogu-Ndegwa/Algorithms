6f22c7749bed60b046c9bc720af9992abc0d38e4
Njogu0001 <dennisndegwa001@gmail.com>
2020-05-18 12:53:29 +0300

    Binary Search

---
+++ binary_search.js
@@ -0,0 +1,27 @@
+const item = 15;
+const array = [2 , 10, 6, 7, 8, 4, 6, 7, 12, 15 ]
+const sorted_array = array.sort(function (a, b){return a-b});
+
+var binarySearch = function binarySearch (arr , item) {
+        let startIndex = 0
+        let endIndex = arr.length - 1;
+
+        while (startIndex <= endIndex) {
+            let middleIndex = Math.floor((startIndex + endIndex)/2 )
+            if(item === arr[middleIndex]){
+                return `The index of the selected item is ${middleIndex}`
+            };
+
+            if(item > arr[middleIndex]) {
+                startIndex = middleIndex + 1 
+            }
+
+            if(item < arr[middleIndex]) {
+                endIndex = middleIndex -1
+            }
+
+        }
+        return null
+}
+
+console.log(binarySearch(sorted_array, item))
--- linear_search.js
+++ linear_search.js
@@ -1,6 +1,6 @@
 
 const item = 'Mango'
-const array = ['Oranges','Mango', 'Wheat' , 'Apples', 'Bananas' , 'Bread']
+const array = ['Oranges','Mango', 'Wheat' , 'Apples', 'Bananas' , 'Bread' , 'Oranges']
 var linearSearch = function linearSearch (array , item) {
     for(let i = 0; i< array.length; i++) {
         if(array[i] ===item ){
@@ -13,3 +13,19 @@ var linearSearch = function linearSearch (array , item) {
 }
 
 console.log('index', linearSearch(array, item))
+
+
+// Global linear search
+
+// var globalSearch = function globalSearch (arr , item) {
+//     let result = []
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] === item) {
+//             result.push(i)
+//             continue;
+//         }
+//     }
+//     return result;
+// }
+
+// console.log('global search', globalSearch(array, item))
